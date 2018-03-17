a7b4a2a82aca38ad0d7561c48cc2313d90602649
unknown <mati1995@onet.pl>
2018-03-17 20:55:02 +0100

    Skrypty do phpka

---
+++ android_login_api/include/Config.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Database config variables
+ */
+define("DB_HOST", "95.211.144.65");
+define("DB_USER", "rootszpital");
+define("DB_PASSWORD", "Administrator1!");
+define("DB_DATABASE", "mtm");
+?>
