8fbdecb12c22b6dd4e1e32ea212d2c8770cd6d37
unknown <mati1995@onet.pl>
2018-03-17 18:55:58 +0100

    Lekkie zmiany

---
+++ SQLite/app/src/main/java/info/androidhive/SQLitepackage/app/AppConfig.java
@@ -0,0 +1,9 @@
+package info.androidhive.SQLitepackage.app;
+
+public class AppConfig {
+	// Server user login url
+	public static String URL_LOGIN = "http://szpital.c0.pl//login.php";
+
+	// Server user register url
+	public static String URL_REGISTER = "http://szpital.c0.pl/register.php";
+}
