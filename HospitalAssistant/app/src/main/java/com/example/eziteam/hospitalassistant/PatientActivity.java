f21e2d2057cbc20bda9eb8c69a29fb4138d27c7c
Cezary S <cezaryseta@gmail.com>
2018-03-21 18:03:45 +0100

    MethodForData: Add sample for Dawid

--- HospitalAssistant/app/src/main/java/com/example/eziteam/hospitalassistant/PatientActivity.java
+++ HospitalAssistant/app/src/main/java/com/example/eziteam/hospitalassistant/PatientActivity.java
@@ -2,12 +2,19 @@ package com.example.eziteam.hospitalassistant;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class PatientActivity extends AppCompatActivity {
-
+    EditText firstname;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patient);
+        firstname = findViewById(R.id.firstnameText);
+        initData();
+    }
+
+    void initData() {
+        firstname.setText("Krzysztof");
     }
 }
