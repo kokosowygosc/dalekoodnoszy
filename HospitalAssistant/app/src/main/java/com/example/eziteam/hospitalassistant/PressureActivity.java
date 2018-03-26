77eaf109601d7f019e61a88fe3056e1c6bf32e3d
Dawid <dawidkadziolka@gmail.com>
2018-03-26 17:31:42 +0200

    Added method to fill fields by data from database to all views

--- HospitalAssistant/app/src/main/java/com/example/eziteam/hospitalassistant/PressureActivity.java
+++ HospitalAssistant/app/src/main/java/com/example/eziteam/hospitalassistant/PressureActivity.java
@@ -4,87 +4,84 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ExpandableListView;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
+
 
 public class PressureActivity extends AppCompatActivity {
 
     private ExpandableListView listView;
     private ExpandableListAdapter listAdapter;
     private List<String> listDataHeader;
-    private HashMap<String,List<String>> listHash;
+    private HashMap<String, List<String>> listHash;
+
+    String data[][] =
+            {
+                    {"Monday", "8:00 140/90", "12:00 140/90", "16:00 140/90", "20:00 140/90"},
+                    {"Tuesday", "8:00 140/90", "12:00 140/90", "16:00 140/90", "20:00 140/90"},
+                    {"Wednesday", "8:00 140/90", "12:00 140/90", "16:00 140/90", "20:00 140/90"},
+                    {"Thursday", "8:00 140/90", "12:00 140/90", "16:00 140/90", "20:00 140/90"},
+                    {"Friday", "8:00 140/90", "12:00 140/90", "16:00 140/90", "20:00 140/90"},
+                    {"Saturday", "8:00 140/90", "12:00 140/90", "16:00 140/90", "20:00 140/90"},
+                    {"Sunday", "8:00 140/90", "12:00 140/90", "16:00 140/90", "20:00 140/90"},
+            };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pressure);
 
-        listView = (ExpandableListView)findViewById(R.id.pressureExpandable);
+        listView = findViewById(R.id.pressureExpandable);
         initData();
-        listAdapter = new ExpandableListAdapter(this,listDataHeader,listHash);
+        listAdapter = new ExpandableListAdapter(this, listDataHeader, listHash);
         listView.setAdapter(listAdapter);
     }
+
 
     private void initData() {
         listDataHeader = new ArrayList<>();
         listHash = new HashMap<>();
-        listDataHeader.add("12.03.18");
-        listDataHeader.add("13.03.18");
-        listDataHeader.add("14.03.18");
-        listDataHeader.add("15.03.18");
-        listDataHeader.add("16.03.18");
-        listDataHeader.add("17.03.18");
-        listDataHeader.add("18.03.18");
-
         List<String> firstDay = new ArrayList<>();
-        firstDay.add("8:00 140/90");
-        firstDay.add("12:00 140/90");
-        firstDay.add("16:00 140/90");
-        firstDay.add("20:00 140/90");
-
         List<String> secondDay = new ArrayList<>();
-        secondDay.add("8:00 140/90");
-        secondDay.add("12:00 140/90");
-        secondDay.add("16:00 140/90");
-        secondDay.add("20:00 140/90");
-
         List<String> thirdDay = new ArrayList<>();
-        thirdDay.add("8:00 140/90");
-        thirdDay.add("12:00 140/90");
-        thirdDay.add("16:00 140/90");
-        thirdDay.add("20:00 140/90");
-
         List<String> fourthDay = new ArrayList<>();
-        fourthDay.add("8:00 140/90");
-        fourthDay.add("12:00 140/90");
-        fourthDay.add("16:00 140/90");
-        fourthDay.add("20:00 140/90");
-
         List<String> fifthDay = new ArrayList<>();
-        fifthDay.add("8:00 140/90");
-        fifthDay.add("12:00 140/90");
-        fifthDay.add("16:00 140/90");
-        fifthDay.add("20:00 140/90");
-
         List<String> sixthDay = new ArrayList<>();
-        sixthDay.add("8:00 140/90");
-        sixthDay.add("12:00 140/90");
-        sixthDay.add("16:00 140/90");
-        sixthDay.add("20:00 140/90");
-
         List<String> seventhDay = new ArrayList<>();
-        seventhDay.add("8:00 140/90");
-        seventhDay.add("12:00 140/90");
-        seventhDay.add("16:00 140/90");
-        seventhDay.add("20:00 140/90");
 
-        listHash.put(listDataHeader.get(0),firstDay);
-        listHash.put(listDataHeader.get(1),secondDay);
-        listHash.put(listDataHeader.get(2),thirdDay);
-        listHash.put(listDataHeader.get(3),fourthDay);
-        listHash.put(listDataHeader.get(4),fifthDay);
-        listHash.put(listDataHeader.get(5),sixthDay);
-        listHash.put(listDataHeader.get(6),seventhDay);
+
+        //TODO Rozdanie danych na listy każdego dnia.
+
+        for (int i = 0; i < data.length; i++) {
+            listDataHeader.add(data[i][0]);
+        }
+
+        for(int i = 0; i <= data.length; i++)
+            for(int j = 0; j < data[0].length; j++)
+            {
+                if(i == 0 && j != 0)
+                    firstDay.add(data[i][j]);
+                if(i == 1 && j != 0)
+                    secondDay.add(data[i][j]);
+                if(i == 2 && j != 0)
+                    thirdDay.add(data[i][j]);
+                if(i == 3 && j != 0)
+                    fourthDay.add(data[i][j]);
+                if(i == 4 && j != 0)
+                    fifthDay.add(data[i][j]);
+                if(i == 5 && j != 0)
+                    sixthDay.add(data[i][j]);
+                if(i == 6 && j != 0)
+                    seventhDay.add(data[i][j]);
+            }
+
+            listHash.put(listDataHeader.get(0), firstDay);
+            listHash.put(listDataHeader.get(1), secondDay);
+            listHash.put(listDataHeader.get(2), thirdDay);
+            listHash.put(listDataHeader.get(3), fourthDay);
+            listHash.put(listDataHeader.get(4), fifthDay);
+            listHash.put(listDataHeader.get(5), sixthDay);
+            listHash.put(listDataHeader.get(6), seventhDay);
+        }
     }
-}
+//}
+
